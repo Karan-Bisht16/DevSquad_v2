c3ac3f19fbfb3e78b8e1ba6c24283a5e297a55a8
Karan Bisht <karan161003@gmail.com>
2023-10-12 07:17:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,328 @@
+const express = require('express');
+const session = require('express-session');
+const path = require("path");
+require('dotenv').config();
+
+const app = express();
+const PORT = 1600;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(__dirname+'/public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+const mongoose = require('mongoose');
+const connection = require('./database');
+connection();
+
+const bcrypt = require('bcrypt');
+const { error } = require('console');
+const saltRounds = 10;
+
+const donation_Schema = new mongoose.Schema({
+    donar_name: {
+        type: String,
+        required: true
+    },
+    date_of_donation: {
+        type: Date,
+        required: true
+    }, 
+    type_of_donation: {
+        type: String,
+        required: true
+    },
+    type_of_event: {
+        type: String
+    },
+    user_mobile_number: {
+        type: Number,
+        required: true
+    }, 
+    user_pickup_address: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true
+    },
+    donated: {
+        type: Boolean,
+        required: true
+    }
+})
+
+const user_Schema = new mongoose.Schema({
+    user_name: {
+        type: String,
+        required: true
+    },
+    user_email: {
+        type: String,
+        required: true
+    }, 
+    user_password: {
+        type: String,
+        required: true
+    },
+    user_donations: Array
+});
+
+const NGO_Schema = new mongoose.Schema({
+    NGO_name: {
+        type: String,
+        required: true
+    },
+    NGO_registration_number: {
+        type: String,
+        required: true
+    }, 
+    NGO_address: {
+        type: String,
+        required: true
+    },
+    NGO_position: {
+        type: mongoose.Schema.Types.Mixed
+    },
+    NGO_webpage: {
+        type: String,
+        required: true
+    }
+});
+
+const NGO = mongoose.model('NGO', NGO_Schema);
+const User = mongoose.model('User', user_Schema);
+const Donation = mongoose.model('Donation', donation_Schema);
+// const ngo = new NGO({
+//     NGO_name: 'HelpAge India',
+//     NGO_registration_number: '9270',
+//     NGO_address: 'C-14 Qutab Institutional Area New Delhi-110016',
+//     NGO_webpage: 'http://www.helpageindia.org'
+// })
+// ngo.save();
+// const userPassword = 'karan@1603';
+// bcrypt.hash(userPassword, saltRounds, (err, hashedPassword) => {
+//     if (err) throw err;
+//     const user = new User({
+//         user_name: 'Karan Bisht',
+//         user_email: 'karan161003@gmail.com',
+//         user_password: hashedPassword
+//     });
+//     user.save();
+//     console.log(hashedPassword);
+// });
+
+// User.find()
+// .then (function(result){
+//     result.forEach(user=>{
+//         console.log(user.user_password);
+//     });
+//     mongoose.disconnect();
+// })
+// .catch (error=>{
+//     console.log("Error: ",error)
+// });
+
+app.get('/', (req, res)=>{
+    // req.session.userID;
+    // console.log(req.session.userID);
+    req.session.location = req.session.location || '';
+    console.log("[GET]  `/`      Current User Location: "+req.session.location);
+    if (req.session.userID) {
+        User.findOne({user_email: req.session.userID})
+        .then (result=>{
+            res.render("home.ejs", {user: result.user_name});
+        })
+        .catch (error=>{
+            console.log("Error: ",error);
+        });
+    } else {
+        res.render("home.ejs");
+    }
+});
+
+app.post('/', (req, res)=>{
+    req.session.location = req.body["latitude"]+'_'+req.body["longitude"];
+    console.log("[POST] `/`      Current User Location: "+req.session.location);
+    req.session.nearbyNGOs = [];
+    Donation.find({donated: false})
+    .then (function(results){
+        results.forEach(result=>{
+            Object.assign(result.user_pickup_address.coordinates, {humanReadableAddress: result.user_pickup_address.humanReadableAddress});
+            Object.assign(result.user_pickup_address.coordinates, {typeOfDonation: result.type_of_donation});
+            req.session.nearbyNGOs.push(result.user_pickup_address.coordinates);
+            console.log(result.user_pickup_address.coordinates);
+        })
+        res.send({data: req.session.nearbyNGOs});
+    })
+    .catch (error=>{
+        console.log("Error: ",error);
+    })
+});
+
+app.get('/donate', (req, res)=>{
+    console.log("[GET] `/donate` Current User Location: "+req.session.location);
+    if (!req.session.userID){
+        res.render('sign_up.ejs', {error:null});
+    } else {
+        User.findOne({user_email: req.session.userID})
+        .then (result=>{
+            if (req.session.location===''){
+                res.render("donate.ejs", {user: result.user_name});
+            } else {
+                res.render("donate.ejs", {user: result.user_name}); //, {currentLocation: req.session.location}
+            }
+        })
+        .catch (error=>{
+            console.log("Error: ",error);
+        });
+    }
+});
+
+app.post('/donate/submit', (req, res)=>{
+    const recievedData = req.body;
+    console.log(req.body);
+    if (recievedData["name"]==='') {
+        res.send({error:'Please enter username'});  
+    } else if (recievedData["dateOfDonation"]==='') {
+        res.send({error:'Please enter date of donation'});  
+    } else if (new Date(recievedData['dateOfDonation'])<new Date()) {
+        res.send({error:'Please enter valid date of donation'})
+    } else if (recievedData["mobileNumber"]==='') {
+        res.send({error:'Please enter mobile number'});  
+    } else if (recievedData['mobileNumber'].length!==10) {
+        res.send({error:'Please enter valid mobile number'});
+    } else if (recievedData["position"]["humanReadableAddress"]==='' || recievedData["position"]["coordinates"]["latitude"]==='') {
+        res.send({error:'Please enter pickup address'});  
+    } else {
+        let donationData = {
+            donar_name: recievedData["name"],
+            date_of_donation: recievedData["dateOfDonation"], 
+            user_mobile_number: recievedData["mobileNumber"], 
+            user_pickup_address: recievedData["position"],
+            donated: false,
+            type_of_donation: recievedData["typeOfDonation"],
+        }
+        if (recievedData["typeOfDonation"]==='Food'){
+            // Object.assign(donationData, {type_of_event: recievedData["typeOfEvent"]});
+            donationData.type_of_event = recievedData["typeOfEvent"];
+        }
+        console.log(donationData);
+        const donation = new Donation(donationData);
+        donation.save()
+        .then ((result)=>{
+            console.log(typeof result._id);
+            console.log(result._id);
+            User.updateOne({user_email: req.session.userID}, {$push: {user_donations: result._id}})
+            .then (()=>{
+                console.log('Done');
+            })
+            .catch (error=>{
+                console.log("Error: ",error);
+            });
+        })
+        .catch(error=>{
+            console.log("Error: ", error);
+        })
+        res.send({error:null});
+        console.log(req.body);
+    }
+});
+
+app.get('/about_us', (req, res)=>{
+    if (req.session.userID) {
+        User.findOne({user_email: req.session.userID})
+        .then (result=>{
+            res.render("about_us.ejs", {user: result.user_name});
+        })
+        .catch (error=>{
+            console.log("Error: ",error);
+        });
+    } else {
+        res.render("about_us.ejs")
+    }
+});
+app.get('/feedback', (req, res)=>{
+    if (req.session.userID) {
+        User.findOne({user_email: req.session.userID})
+        .then (result=>{
+            res.render("feedback.ejs", {user: result.user_name});
+        })
+        .catch (error=>{
+            console.log("Error: ",error);
+        });
+    } else {
+        res.render("feedback.ejs");
+    }
+});
+app.get('/sign_up', (req, res)=>{
+    res.render("sign_up.ejs", {error: null})
+});
+app.post('/sign_up',(req, res)=>{
+    //assuming email to be unique
+    // let newUser;
+    bcrypt.hash(req.body["userPassword"], saltRounds, (err, hashedPassword) => {
+        if (err) throw err;
+        var newUser = new User({
+            user_name: req.body["userName"],
+            user_email: req.body["userEmail"],
+            user_password: hashedPassword
+        });
+        newUser.save()
+        .then (result=>{
+            req.session.userID = req.body["userEmail"];
+            console.log("[POST] `/sign_up`",req.session.userID);
+            res.redirect('/');
+        })
+        .catch (error=>{
+            console.log("Error: ",error);
+            res.redirect('/sign_up')
+        });
+    });
+});
+
+app.post('/login', (req, res)=>{
+    // console.log(req.body);
+    User.findOne({user_email: req.body["userEmail"]})
+    .then (user=>{
+        if (user) {
+            // console.log('Found User:', user);
+            bcrypt.compare(req.body["userPassword"], user.user_password, (err, result) => {
+                if (err) throw err;
+                if (result) {
+                    // console.log('Login successful');
+                    req.session.userID = req.body["userEmail"];
+                    res.redirect('/');
+                } else {
+                    // console.log('Invalid password');
+                    res.render('sign_up.ejs', {error: 'Invalid Password'});
+                }
+            });
+        } else {
+            // console.log('User not found');
+            res.render('sign_up.ejs', {error: 'User not found'});
+        }
+    })
+    .catch (err=>{
+        console.error('Error:', err);
+    });
+});
+
+app.get('/logout', (req, res)=>{
+    req.session.destroy(err => {
+        if (err) {
+            console.error('Session destruction error:', err);
+            return;
+        }
+        res.redirect('/');
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ public/js/script_home.js
@@ -0,0 +1,125 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $('.title-card').each(function() {
+            var elementTop = $(this).offset().top;
+            var viewportTop = $(window).scrollTop();
+            if (elementTop < viewportTop + window.innerHeight/1.25) {
+                $(this).addClass('fade-in');
+            }
+        });
+    });
+});
+
+let lat=28.6139, lng=77.2090, marker, circle, zoomed;
+var map = L.map('map').setView([lat, lng], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 16,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const streetsLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+const terrainLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+    attribution: '&copy; <a href="https://stamen.com">Stamen Design</a> contributors',
+    maxZoom: 14,
+    ext: 'png'
+});
+
+const osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 14,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+const baseLayers = {
+    'Streets': streetsLayer,
+    'Terrain': terrainLayer,
+    'OSM': osmHOT
+};
+
+L.control.layers(baseLayers).addTo(map);
+
+navigator.geolocation.watchPosition(success, error);
+
+async function sendPostionToServer(object) {
+    const currentURL = window.location.href;
+    try {
+        const response = await fetch(currentURL, {
+            method: 'POST', 
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(object)
+        });
+        const url = await response.json();
+        if (url["data"].length>0) {
+            let nearbyNGOsArray = url["data"];
+            const customIconNGO = L.icon({
+                iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Green_LocationMarker.png',
+                iconSize: [30, 30]
+            })
+            const customIconToy = L.icon({
+                iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/80/Red_LocationMarker.png',
+                iconSize: [30, 30]
+            })
+            const customIconBook = L.icon({
+                iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/df/Amber_LocationMarker.png',
+                iconSize: [30, 30]
+            })
+    
+            for (var i=0; i<url["data"].length; i++) {
+                let customLat = parseFloat(nearbyNGOsArray[i]['latitude']).toFixed(5);
+                let customLng = parseFloat(nearbyNGOsArray[i]['longitude']).toFixed(5);
+                let type = nearbyNGOsArray[i]['typeOfDonation'];
+                if (type==='Food') {
+                    const Toys = L.marker([customLat, customLng], {icon: customIconToy}).addTo(map);
+                    Toys.bindPopup(`Food donation at ${nearbyNGOsArray[i]['humanReadableAddress']}`);
+                } else {
+                    const Books = L.marker([customLat, customLng], {icon: customIconBook}).addTo(map);
+                    Books.bindPopup(`Book donation at ${nearbyNGOsArray[i]['humanReadableAddress']}`);
+                }
+            }
+        } else {
+            console.error("Server side error")
+        }
+    } catch (error) {
+        console.error("Error in adding to server ",error);
+    }
+}
+
+function success(position) {
+    if (Math.abs(lat-position.coords.latitude) || Math.abs(lng-position.coords.longitude)){
+        let positionObj = {
+            latitude: position.coords.latitude, 
+            longitude: position.coords.longitude
+        };
+        sendPostionToServer(positionObj);
+    }
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+    const acc = position.coords.accuracy;
+    
+    if (marker) {
+        map.removeLayer(marker);
+        map.removeLayer(circle);
+    }
+    marker = L.marker([lat,lng]).addTo(map);
+    circle = L.circle([lat,lng,{radius: acc}]).addTo(map);
+    
+    if (!zoomed) {
+        zoomed = map.fitBounds(circle.getBounds());
+    }
+}
+
+function error() {
+    if (error.code === 1) {
+        alert("Allow geolocation access");
+    } else {
+        console.log("Cannot get current location");
+    }
+}
+
+const originalPosBtn =  document.querySelector('#originalPosition');
+originalPosBtn.addEventListener('click', ()=>{
+    map.setView([lat, lng]);
+});
