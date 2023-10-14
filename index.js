1fe91da3d2a57840d3b9176f462ec6f5b2959102
Karan Bisht <karan161003@gmail.com>
2023-10-14 21:07:00 +0530

    modified:   index.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const path = require("path");
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 const app = express();
